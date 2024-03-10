2193c16818e03306bdac7ffdc97a21ab55561504
Ilia Burakov <burakov.ilia@gmail.com>
2024-03-10 18:43:11 -0400

    remove forecast from API query

--- WeatherTgBot/WeatherTgBot/Abstractions/IOpenMeteoApi.cs
+++ WeatherTgBot/WeatherTgBot/Abstractions/IOpenMeteoApi.cs
@@ -5,6 +5,6 @@ namespace WeatherTgBot.Abstractions;
 public interface IOpenMeteoApi
 {
     // see https://open-meteo.com/en/docs/
-    [Get("/v1/forecast?latitude={latitudes}&longitude={longitudes}&timezone={timezones}&current=is_day,temperature_2m,relative_humidity_2m,precipitation,rain,showers,snowfall,cloud_cover,wind_speed_10m&daily=temperature_2m_max,temperature_2m_min,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,precipitation_probability_max,wind_speed_10m_max")]
+    [Get("/v1/forecast?latitude={latitudes}&longitude={longitudes}&timezone={timezones}&current=temperature_2m,relative_humidity_2m,precipitation,rain,showers,snowfall,cloud_cover,wind_speed_10m&hourly=&daily=&forecast_days=1")]
     Task<string> GetWeatherAsync(string latitudes, string longitudes, string timezones, CancellationToken cancellationToken);
 }
